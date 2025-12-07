1a5e5c30cb6724f726fe32778fca340448d2b2a3
oryacobi <36107677+oryacobi@users.noreply.github.com>
2025-12-07 06:28:44 -0800

    Add smoke test for common package import (#2)

---
+++ tests/test_smoke.py
@@ -0,0 +1,10 @@
+"""Basic smoke tests for the common package."""
+
+def test_import_common():
+    import common
+
+    assert common is not None
+
+
+def test_truthiness():
+    assert True is True
